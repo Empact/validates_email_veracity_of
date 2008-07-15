645acd9e439f8067202deea5612566af032bcc90
Ben Woosley <ben.woosley@gmail.com>
2008-07-15 02:39:14 -0700

    Prefer Rails 2.1

--- test/rails_root/config/environment.rb
+++ test/rails_root/config/environment.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 Rails::Initializer.run do |config|
   config.log_level = :debug
